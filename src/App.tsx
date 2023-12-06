31eddf56d737f2d28e388851d612c8f80e1d210c
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2023-12-06 01:02:41 -0500

    Card component and Profile class

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <>
       <Header />
-      <Card />
+      <Card
+        name="GrumpyCoder"
+        description="I am software developper and love to learn. Be careful, I can be grumpy."
+      />
+      <Card
+        name="Michelle Pinar" 
+        description="GrumpyCoder's wife. Determined to create the Grumpy's dream doudou."
+      />
+      <Card
+        name="Minou Montpetit"
+        description="GrumpyCoder's & Michelle's son. Passionnate about croquettes and cozy spots."
+      />
       <Footer />
     </>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,18 +1,67 @@
-import ProfilePicture from "./assets/grumpy-coder-2.png";
+import GrumpyCoderPicture from "./assets/grumpy-coder-2.png";
+import MichellePicture from "./assets/wife-1.jpg";
+import MinouPicture from "./assets/minou-1.jpg";
 
-function Card() {
+class Profile {
+  private name: string = "";
+  private picture: string = "";
+  private description: string = "";
+  private static totalProfiles = 0;
+
+  constructor(name: string, description: string) {
+    this.name = name;
+    this.description = description;
+
+    if (name === "GrumpyCoder") {
+      this.picture = GrumpyCoderPicture;
+    } else if (name === "Michelle Pinar") {
+      this.picture = MichellePicture;
+    } else if (name === "Minou Montpetit") {
+      this.picture = MinouPicture;
+    }
+    Profile.totalProfiles++;
+  }
+
+  public getName() {
+    return this.name;
+  }
+  public setName(name: string) {
+    this.name = name;
+  }
+  public getPicture() {
+    return this.picture;
+  }
+  public setPicture(picture: string) {
+    this.picture = picture;
+  }
+  public getDescription() {
+    return this.description;
+  }
+  public setDescription(description: string) {
+    this.description = description;
+  }
+  public static getTotalProfiles() {
+    return this.totalProfiles;
+  }
+  public static incrementTotalProfiles() {
+    return this.totalProfiles++;
+  }
+}
+
+function Card(props: any) {
+  const profile = new Profile(props.name, props.description);
+  console.log(profile);
   return (
     <div className="card">
       <img
         className="card-profile-image"
-        src={ProfilePicture}
+        src={profile.getPicture()}
         alt="profile-picture"
       >
       </img>
-      <h2 className="card-profile-name">GrumpyCoder</h2>
+      <h2 className="card-profile-name">{profile.getName()}</h2>
       <p className="card-profile-description">
-        I am software developper and love to learn. <br />
-        Be careful, I can be grumpy.
+        {profile.getDescription()}
       </p>
     </div>
   );
